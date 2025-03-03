3e27529cc372c1e80b58af61d652e09285c7e3d0
mohamedelsayed148 <me773631@gmail.com>
2025-03-03 12:46:37 +0200

    fix mailtrap import error

--- backend/emails/emailHandlers.js
+++ backend/emails/emailHandlers.js
@@ -1,4 +1,4 @@
-import { MailtrapClient, sender } from "../lib/mailtrap.js";
+import { mailtrapClient, sender } from "../lib/mailtrap.js";
 
 export const sendWelcomeEmail = async (email, name, profileUrl) => {
   const recipient = [{ email }];
@@ -13,9 +13,7 @@ export const sendWelcomeEmail = async (email, name, profileUrl) => {
     });
 
     console.log("welcome email sent successfully: ", response);
-
   } catch (error) {
     throw error;
   }
-
 };

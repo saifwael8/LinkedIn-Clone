4b3b2861d3d148d410347136ef77537e368347eb
mohamedelsayed148 <me773631@gmail.com>
2025-03-09 21:23:36 +0200

    Fix errors in Navbar , signup , Tailwind, Layout

--- frontend/src/pages/auth/SignUpPage.jsx
+++ frontend/src/pages/auth/SignUpPage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import SignUpForm from "../../components/layout/auth/SignUpForm";
 
 const SignUpPage = () => {
   return (
-    <div>SignUpPage</div>
-  )
-}
+    <div>
+      <h1>SignUpPage</h1>
+      <SignUpForm />
+    </div>
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
